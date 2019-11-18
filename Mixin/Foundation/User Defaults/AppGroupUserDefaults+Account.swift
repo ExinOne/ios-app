56daa7f7469a252fb83b460481e115857530d1ed
wuyueyang <wuyueyang@mixin.one>
2019-11-18 17:50:50 +0800

    Migrate AccountUserDefault

--- Mixin/Foundation/User Defaults/AppGroupUserDefaults+Account.swift
+++ Mixin/Foundation/User Defaults/AppGroupUserDefaults+Account.swift
@@ -10,7 +10,7 @@ extension AppGroupUserDefaults {
             case pinToken = "pin_token"
             case isClockSkewed = "clock_skew"
             case canRestoreChat = "can_restore_chat"
-            case canRestoreFilesAndVideo = "can_restore_file_videos"
+            case canRestoreMedia = "can_restore_media"
             case hasUnfinishedBackup = "has_unfinished_backup"
             case extensionSession = "extension_session"
             case lastDesktopLoginDate = "last_desktop_login_date"
@@ -31,8 +31,8 @@ extension AppGroupUserDefaults {
         @Default(namespace: .account, key: Key.canRestoreChat, defaultValue: false)
         public static var canRestoreChat: Bool
         
-        @Default(namespace: .account, key: Key.canRestoreFilesAndVideo, defaultValue: false)
-        public static var canRestoreFilesAndVideo: Bool
+        @Default(namespace: .account, key: Key.canRestoreMedia, defaultValue: false)
+        public static var canRestoreMedia: Bool
         
         @Default(namespace: .account, key: Key.hasUnfinishedBackup, defaultValue: false)
         public static var hasUnfinishedBackup: Bool
@@ -43,13 +43,27 @@ extension AppGroupUserDefaults {
         @NullableDefault(namespace: .account, key: Key.lastDesktopLoginDate, defaultValue: nil)
         public static var lastDesktopLoginDate: Date?
         
+        public static var isDesktopLoggedIn: Bool {
+            guard let sessionId = extensionSession else {
+                return false
+            }
+            return !sessionId.isEmpty
+        }
+        
+        public static func clearAll() {
+            Key.allCases
+                .map({ $0.rawValue })
+                .forEach(defaults.removeObject(forKey:))
+            defaults.synchronize()
+        }
+        
         internal static func migrate() {
             serializedAccount = AccountUserDefault.shared.serializedAccount
             sessionSecret = AccountUserDefault.shared.getToken()
             pinToken = AccountUserDefault.shared.getPinToken()
             isClockSkewed = AccountUserDefault.shared.hasClockSkew
             canRestoreChat = AccountUserDefault.shared.hasRestoreChat
-            canRestoreFilesAndVideo = AccountUserDefault.shared.hasRestoreMedia
+            canRestoreMedia = AccountUserDefault.shared.hasRestoreMedia
             hasUnfinishedBackup = AccountUserDefault.shared.hasRebackup
             extensionSession = AccountUserDefault.shared.extensionSession
             lastDesktopLoginDate = AccountUserDefault.shared.lastDesktopLogin
